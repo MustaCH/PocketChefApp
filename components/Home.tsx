320b17f6c3897fe2e7932ffb3664c0538b9977ff
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-05-09 12:37:58 -0300

    FEATURE | Removed Flatlist

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,7 +3,7 @@ import {
   Text,
   TextInput,
   ActivityIndicator,
-  FlatList,
+  ScrollView,
 } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useState } from "react";
@@ -52,62 +52,114 @@ export default function Home() {
   );
 
   return (
-    <View style={{ padding: 16, paddingTop: insets.top + 16 }}>
-      <View className="flex flex-col gap-2 m-2">
-        <View className="flex flex-col gap-2">
-          <Text className="text-2xl font-bold">¿Qué ingredientes tienes?</Text>
-          <Text className="text-sm text-gray-500">
-            Ingresa los ingredientes que tienes en tu cocina y nosotros
-            generaremos recetas deliciosas para ti!
-          </Text>
-        </View>
-        <View className="flex flex-col gap-4 mt-4">
-          <View>
-            <Text>Ingredientes</Text>
-            <TextInput
-              className="border border-gray-300 bg-gray-200 rounded-lg p-2 mt-2"
-              placeholder="e., pollo, arroz, brocoli"
-              placeholderTextColor="#888"
-              value={ingredients}
-              onChangeText={setIngredients}
-            />
+    <ScrollView
+      className="flex"
+      style={{
+        paddingBottom: insets.bottom,
+        paddingTop: insets.top + 16,
+      }}
+    >
+      <View
+        className="flex flex-col gap-2 m-4 border border-gray-200 rounded-lg p-4"
+        style={{
+          boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+          shadowOffset: { width: 0, height: 2 },
+          shadowColor: "#000",
+          shadowOpacity: 0.1,
+          shadowRadius: 10,
+        }}
+      >
+        {recipes.length === 0 && (
+          <View className="flex flex-col gap-2">
+            <Text className="text-2xl font-bold">
+              ¿Qué ingredientes tienes?
+            </Text>
+            <Text className="text-sm text-gray-500">
+              Ingresa los ingredientes que tienes en tu cocina y nosotros
+              generaremos recetas deliciosas para ti!
+            </Text>
           </View>
-          <View>
-            <Text>Restricciones dietarias (Opcional)</Text>
-            <TextInput
-              className="border border-gray-300 bg-gray-200 rounded-lg p-2 mt-2"
-              placeholder="e., vegano, libre de gluten"
-              placeholderTextColor="#888"
-              value={restrictions}
-              onChangeText={setRestrictions}
-            />
+        )}
+        {recipes.length === 0 ? (
+          <View className="flex flex-col gap-4 mt-4">
+            <View>
+              <Text>Ingredientes</Text>
+              <TextInput
+                className="border border-gray-300 bg-gray-200 rounded-lg p-2 mt-2"
+                placeholder="e., pollo, arroz, brocoli"
+                placeholderTextColor="#888"
+                value={ingredients}
+                onChangeText={setIngredients}
+              />
+            </View>
+            <View>
+              <Text>Restricciones dietarias (Opcional)</Text>
+              <TextInput
+                className="border border-gray-300 bg-gray-200 rounded-lg p-2 mt-2"
+                placeholder="e., vegano, libre de gluten"
+                placeholderTextColor="#888"
+                value={restrictions}
+                onChangeText={setRestrictions}
+              />
+            </View>
           </View>
-          <View>
+        ) : (
+          <View className="flex flex-col gap-4 my-4">
+            <Text className="text-xl font-bold">
+              Recetacetas generadas con:
+            </Text>
+            <Text className="text-lg font-semibold">{ingredients}</Text>
+            {restrictions && (
+              <Text className="text-lg font-bold">{restrictions}</Text>
+            )}
+          </View>
+        )}
+        <View className="flex flex-row justify-between gap-2">
+          <Button
+            buttonStyle={`bg-blue-500 p-6 rounded-lg ${
+              recipes.length > 0 ? "w-1/2" : "w-full"
+            }`}
+            textStyle="text-white text-lg font-semibold text-center"
+            title={
+              loading
+                ? "Generando..."
+                : recipes.length > 0
+                ? "Regenerar"
+                : "Generar recetas"
+            }
+            onPress={handleSubmit}
+            disabled={loading}
+          />
+          {recipes.length > 0 && (
             <Button
-              buttonStyle="bg-blue-500 p-6 rounded-lg"
+              buttonStyle="bg-red-500 p-6 rounded-lg w-1/2"
               textStyle="text-white text-lg font-semibold text-center"
-              title={loading ? "Generando..." : "Generar recetas"}
-              onPress={handleSubmit}
-              disabled={loading}
+              title="Borrar recetas"
+              onPress={() => setRecipes([])}
             />
-          </View>
+          )}
         </View>
       </View>
       {loading && <ActivityIndicator size="large" color="#2563eb" />}
       {error ? (
         <Text className="text-red-500 mt-4 text-center">{error}</Text>
       ) : null}
-      <View className="p-6">
-        {recipes.length > 0 && (
-          <FlatList
-            data={recipes}
-            renderItem={renderRecipe}
-            keyExtractor={(_, index) => index.toString()}
-            contentContainerStyle={{ paddingBottom: 20 }}
-            showsVerticalScrollIndicator={false}
-          />
-        )}
-      </View>
-    </View>
+      {recipes.length > 0 && (
+        <View
+          style={{ paddingHorizontal: 16, marginBottom: insets.bottom + 60 }}
+        >
+          {recipes.map((item, index) => (
+            <View key={index} style={{ marginBottom: 16 }}>
+              <RecipeCard
+                name={item.name || "Receta sin nombre"}
+                instructions={item.instructions || ""}
+                availableIngredientsUsed={item.availableIngredientsUsed || []}
+                ingredientsRequired={item.ingredientsRequired || []}
+              />
+            </View>
+          ))}
+        </View>
+      )}
+    </ScrollView>
   );
 }
--- components/ui/RecipeCard.tsx
+++ components/ui/RecipeCard.tsx
@@ -15,11 +15,20 @@ export const RecipeCard = ({
   ingredientsRequired,
 }: RecipeCardProps) => {
   return (
-    <View className="bg-white p-4 rounded-lg border border-gray-200">
+    <View
+      className="bg-white p-4 rounded-lg border border-gray-200"
+      style={{
+        boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+        shadowOffset: { width: 0, height: 2 },
+        shadowColor: "#000",
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+      }}
+    >
       <Text className="text-2xl font-bold mb-2">{name}</Text>
 
       {availableIngredientsUsed && availableIngredientsUsed.length > 0 && (
-        <View className="flex flex-col gap-2">
+        <View className="flex flex-col mb-4">
           <Text className="text-lg font-bold">Ingredientes disponibles:</Text>
           <Text className="text-sm text-gray-500">
             {availableIngredientsUsed.join(", ")}
@@ -28,7 +37,7 @@ export const RecipeCard = ({
       )}
 
       {ingredientsRequired && ingredientsRequired.length > 0 && (
-        <View className="flex flex-col gap-2">
+        <View className="flex flex-col mb-4">
           <Text className="text-lg font-bold">Ingredientes adicionales:</Text>
           <Text className="text-sm text-gray-500">
             {ingredientsRequired.join(", ")}
@@ -36,7 +45,7 @@ export const RecipeCard = ({
         </View>
       )}
 
-      <View className="flex flex-col gap-2">
+      <View className="flex flex-col">
         <Text className="text-lg font-bold">Instrucciones:</Text>
         <Text className="text-sm text-gray-500">{instructions}</Text>
       </View>
